f1cf388cab27749f929e30f3b0aff603df557031
abdielp06 <rodrigo.puente@fi.unam.edu>
2023-11-16 12:12:37 -0600

    Subido con dumento

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
-from scanner import *
-from parseer import *
+from Lexer import *
+from parser_1 import *
+import sys
+
+
 
 def main():
-    intro = """
-        \n\t\tAnalizador Lexico . Proyecto 2 compiladores 
-    """
-    print(intro)
+    print("\n\t\tParser . Proyecto 2 compiladores")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: el compilador necesita el archivo fuente como argumento.")
+    with open(sys.argv[1], 'r') as inputFile:
+        source = inputFile.read()
+
     
-    
-    codigo = """
-        IMPRIMIR "hello world"
-        IMPRIMIR "second line" 
-    """ 
-    codigo = codigo + "\n"     
-    scanner = Scanner(codigo)
-    print(f' A continuacion se muestra el codigo ingresado: \n {codigo} \n\n\nAnalizando ...')
-    print("\nSe ha analizado el codigo ingresado, retornando los siguientes tokens:\n")
+    lexer = Lexer(source)
+    parser = Parser(lexer)
 
-    '''
-    token = scanner.caracterAToken() 
-    while token.tipoToken != TipodeTokens.FIN_DE_LINEA: 
-        print(f"\tLeido\t\t{token.caracterToken}\t\t----Tokenizado a --->\t\t{token.tipoToken} \n")
-        token = scanner.caracterAToken()
- 
-    print("\n\n Analizador Lexico finalizado  \n")
-
-    print('Iniciando analisis sintactico')
-    '''
-    scanner = Scanner(codigo)
-    parser = Parser(scanner)
-
-    parser.analizaEstructura()  
-    print("Parser completado")
+    parser.programa() 
+    print()
 
 main()
